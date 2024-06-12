bf6eaa3118e2ab7b19606d8802f73af3c0a41672
udkreddy <udkr1996@gmail.com>
2024-06-11 23:22:17 -0500

    Updated data json

--- src/pages/surveys.js
+++ src/pages/surveys.js
@@ -2,21 +2,12 @@
 
 import React, { useState } from 'react';
 import './surveys.css';
+import {data} from './data';
 const Surveys = () => {
 
     const [activeTab, setActiveTab] = useState(null);
 
-  const tabs = [
-    { id: 1, title: 'Paper 1', description: 'This is a short description for Tab 1.' },
-    { id: 2, title: 'Paper 2', description: 'This is a short description for Tab 2.' },
-    { id: 3, title: 'Paper 3', description: 'This is a short description for Tab 3.' },
-    { id: 4, title: 'Paper 4', description: 'This is a short description for Tab 4.' },
-    { id: 5, title: 'Paper 5', description: 'This is a short description for Tab 5.' },
-    { id: 6, title: 'Paper 6', description: 'This is a short description for Tab 6.' },
-    { id: 7, title: 'Paper 7', description: 'This is a short description for Tab 7.' },
-    { id: 8, title: 'Paper 8', description: 'This is a short description for Tab 8.' },
-    { id: 9, title: 'Paper 9', description: 'This is a short description for Tab 9.' },
-  ];
+  const tabs = data.surveyList;
   return (
     <div className="tabs-page">
       {tabs.map(tab => (
